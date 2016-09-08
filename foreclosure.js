3d954e8992671af362c6531211c29165ff3fb3f8
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-08 09:06:21 -1000

    messing with hoisting

--- foreclosure.js
+++ foreclosure.js
@@ -4,7 +4,33 @@ var stevesLoan;
 var month = 0;
 var monthsUntilEvicted;
 
-function loan() {
+let borrower = function(loan) {
+  let account = {
+    monthlyIncome : 1350,
+    funds : 2800,
+    loan : loan
+  };
+  return {
+    getFunds : function() {
+      return account.funds;
+    },
+    makePayment : function() {
+      if (account.funds > loan.getMonthlyPayment())
+      {
+        account.funds -= loan.getMonthlyPayment();
+        loan.receivePayment(loan.getMonthlyPayment());
+      } else {
+        loan.receivePayment(account.funds);
+        account.funds = 0;
+      }
+    },
+    payDay : function() {
+      account.funds += account.monthlyIncome;
+    }
+  };
+};
+
+let loan = function() {
   let account = {
     borrowed : 550000,
     balance : 286000,
@@ -33,33 +59,7 @@ function loan() {
       return account.foreclosed;
     }
   };
-}
-
-function borrower(loan) {
-  let account = {
-    monthlyIncome : 1350,
-    funds : 2800,
-    loan : loan
-  };
-  return {
-    getFunds : function() {
-      return account.funds;
-    },
-    makePayment : function() {
-      if (account.funds > loan.getMonthlyPayment())
-      {
-        account.funds -= loan.getMonthlyPayment();
-        loan.receivePayment(loan.getMonthlyPayment());
-      } else {
-        loan.receivePayment(account.funds);
-        account.funds = 0;
-      }
-    },
-    payDay : function() {
-      account.funds += account.monthlyIncome;
-    }
-  };
-}
+};
 
 stevesLoan = loan();
 steve = borrower(stevesLoan);
